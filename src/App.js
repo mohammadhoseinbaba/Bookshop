7ce6a8ed02495604767d9e3dea66d196800f8a24
MRX <sirhb77@gmail.com>
2024-10-27 18:44:56 +0100

    updating array state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ function App (){
 const [books,setBooks]=useState([]);
 
 const createBook=(title)=>{
-    console.log('Need to add book with :',title)
+    const updateBooks=[
+        ...books,
+        {id:123,title}
+    ]
+    setBooks(updateBooks)
 }
 
     return <div><BookCreate onCreate={createBook}/></div>
