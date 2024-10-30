b8269bb62ebec853da86a43c5f97e81922c4a198
MRX <sirhb77@gmail.com>
2024-10-30 11:00:52 +0100

    adding provider with useContext

--- src/Component/BookList.js
+++ src/Component/BookList.js
@@ -1,9 +1,18 @@
 import BookShow from "./BookShow"
+import { useContext } from "react"
+import BooksContext from "../Context/Books"
 
 function BookList ({books, onDelete,onEdit}){
+const num = useContext(BooksContext)
+
     const renderedbooks= books.map((book)=>{
 return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book}/>})
 
-    return <div>{renderedbooks}</div>
+    return( 
+        <div>
+        <div>{num}</div>
+        <div>{renderedbooks}</div>
+     </div>
+)
 }
 export default BookList
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.js'
+import BooksContext from './Context/Books.js'
 
 const el = document.getElementById('root')
 const root = ReactDOM.createRoot(el)
-root.render(<App />)
+root.render(
+<BooksContext.Provider value={5}>
+<App />
+</BooksContext.Provider>
+)
